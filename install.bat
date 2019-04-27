90fa40f6261fc392970c8da91c26d62bcd124174
Emil Djupfeldt <emil@djupfeldt.se>
2019-04-27 09:24:49 +0200

    Remove install script as it is no longer needed with the builtin steam workshop support in the game

--- install.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set "ONI_PATH=D:\SteamLibrary\steamapps\common\OxygenNotIncluded"
-set /p "ONI_PATH=Enter Oxygen Not Included installation dir [%ONI_PATH%]"
-
-xcopy StreamingAssets "%ONI_PATH%\OxygenNotIncluded_Data\StreamingAssets" /i /s /e /y
-type NUL >> "%ONI_PATH%\OxygenNotIncluded_Data\debug_enable.txt"
